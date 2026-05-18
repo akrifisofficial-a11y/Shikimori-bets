defcb5a911eb91890fb0bcfc38ae855db66f8c57
akrifisofficial-a11y <akrifisofficial@gmail.com>
2026-05-18 11:16:13 +0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'https://твой-project.supabase.co';
-const SUPABASE_KEY = 'твой-anon-key';
+const SUPABASE_URL = 'https://jgixjjycdnfhtkbzsmlf.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpnaXhqanljZG5maHRrYnpzbWxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwNzY4MjQsImV4cCI6MjA5NDY1MjgyNH0.vEY5NV3iQS-7Okg-STQjHW87kVz9ai3cJZgfsW1OJJQ';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
